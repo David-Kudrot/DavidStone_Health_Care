97251118723fc8dfbfeecddc2b8753784d346aa8
David Kudrot <davidkudrot@gmail.com>
2024-01-22 00:37:34 +0530

    Added Models

--- patient/admin.py
+++ patient/admin.py
@@ -9,5 +9,7 @@ class PatientAdmin(admin.ModelAdmin):
         return obj.user.first_name
     def last_name(self, obj):
         return obj.user.last_name
+    def email(self, obj):
+        return obj.user.email
 
 admin.site.register(Patient, PatientAdmin)
