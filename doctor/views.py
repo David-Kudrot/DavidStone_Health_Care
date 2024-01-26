ce219f3d398893f59e8973387ad4952761f8ce4c
David Kudrot <davidkudrot@gmail.com>
2024-01-26 11:47:59 +0530

    Filter added

--- doctor/views.py
+++ doctor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, pagination
+from rest_framework import viewsets, pagination, filters
 from . import models
 from . import serializers
 # Create your views here.
@@ -25,10 +25,23 @@ class SpecializationViewset(viewsets.ModelViewSet):
 class DesignationViewset(viewsets.ModelViewSet):
     queryset = models.Designation.objects.all()
     serializer_class = serializers.DesignationSerializer
+
+
+
+# particular doctor id er jonno Available Time filter korbo, tai django filter use kortesi, installed app a add kora lagbe
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, query_set, view):
+        doctor_id = request.query_params.get("doctor_id") # patient er jonno jemon kora hoise same ekahne 
+        if doctor_id:
+            return query_set.filter(doctor = doctor_id) # Doctor model ache tai eta automatic small doctor likha jabe, jehetu related_name use korinai 
+        return query_set
     
+        
 class AvailableTimeViewset(viewsets.ModelViewSet):
     queryset = models.AvailableTime.objects.all()
     serializer_class = serializers.AvailableTimeSerializer
+    # filter korar jonno filter_backends built-in variable use kora holo
+    filter_backends = [AvailableTimeForSpecificDoctor]
     
 class ReviewViewset(viewsets.ModelViewSet):
     queryset = models.Review.objects.all()
